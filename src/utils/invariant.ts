965e79051bbb6d54f7c6898458108c6cae96ebdb
akamfoad <akamfanaye@gmail.com>
2023-09-20 12:48:46 +0300

    fix: simplify invariant and make it consistent

--- src/utils/invariant.ts
+++ src/utils/invariant.ts
@@ -1,13 +1,8 @@
-const isProduction: boolean = process.env.NODE_ENV === 'production';
-const prefix = 'Invariant failed';
-
 export function invariant(
   condition: boolean,
   message: string,
 ): asserts condition {
   if (condition) return;
 
-  if (isProduction) throw new Error(prefix);
-
-  throw new Error(message ? `${prefix}: ${message}` : prefix);
+  throw new Error(message);
 }
